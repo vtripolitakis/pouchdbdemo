1c56875f7a66f9e5aee9b54dc59e38e6c8092de3
Vaggelis <vtripolitakis@gmail.com>
2017-04-11 15:39:54 +0300

    [ADD] simple working example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-"use strict"
+var PouchDB = require("pouchdb")
+var db = new PouchDB("koko")
+
+
+function showDocs(){
+    db.allDocs({
+        include_docs: true,
+        attachments: true
+    }).then(function (result) {
+        let data = ""
+        result.rows.map((obj)=>{
+            data+=JSON.stringify(obj)+"<br/>"
+        })
+        document.getElementById("results").innerHTML = data
+    }).catch(function (err) {
+        document.getElementById("results").innerHTML = err
+    })
+}
+
+function addUser(){
+    const slug = document.getElementById("slug")
+    const fullname = document.getElementById("fullname")
+    const age = document.getElementById("age")
+    const execution_result = document.getElementById("execution_result")
+
+    db.put({
+        _id: slug.value,
+        fullname : fullname.value,
+        age : age.value
+    }).then(() => {
+        execution_result.innerHTML = "<strong style='color:green'>Inserted successfully</strong>"
+        slug.value = ""
+        fullname.value = ""
+        age.value = 0
+    }).catch((err) => {
+        execution_result.innerHTML = "<strong style='color:red'>"+err+"</strong>"
+    })
+}
+
+document.getElementById("showBtn").addEventListener("click",showDocs)
+document.getElementById("addBtn").addEventListener("click",addUser)
+
 
 
